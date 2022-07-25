6b6aafd0bc07860e3b0b3ed3db6ef2cfd91513c9
CarlG96 <carl.gardiner1@hotmail.com>
2022-07-25 15:44:31 +0000

    third commit for learning

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -2,7 +2,11 @@ const { TestScheduler } = require("jest");
 const buttonClick = require("../button");
 
 beforeEach( () => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 
 });
 
@@ -10,5 +14,8 @@ describe("DOM tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("Check if h1 tag exists", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
