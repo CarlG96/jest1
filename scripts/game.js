25e896fa9b305c42b663ba8482ae7e229a3f5936
CarlG96 <carl.gardiner1@hotmail.com>
2022-07-26 12:42:37 +0000

    second commit for simon game

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,25 @@
 let game = {
-    score: 0
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
-module.exports = { game };
+function newGame() {
+    game.score = 0;
+    game.playerMoves.length = 0;
+    game.currentGame.length = 0;
+    showScore();
+    addTurn();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function addTurn() {
+    game.playerMoves.length = 0;
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns
+}
+module.exports = { game, newGame, showScore, addTurn };
